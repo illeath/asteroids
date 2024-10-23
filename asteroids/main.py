13f8e28de1a2ed481f1bcf4e234c31ff098f94ed
illeath <illeath01@gmail.com>
2024-10-23 12:15:05 +0200

    confirm changes

---
+++ asteroids/main.py
@@ -0,0 +1,11 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
