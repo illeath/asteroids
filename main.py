dd67e9df627cad4988165466d490dca775582d7c
illeath <illeath01@gmail.com>
2024-10-23 12:17:49 +0200

    moves were made

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from constants import *
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"screen height: {SCREEN_HEIGHT}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
